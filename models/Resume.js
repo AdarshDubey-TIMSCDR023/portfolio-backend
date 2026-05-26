73fd6e9825d4bc42c34b064cedffcaaf6b8982e2
AdarshDubey-TIMSCDR023 <mca-reg202527023@timscdrmumbai.in>
2026-05-27 00:56:46 +0530

    Update Resume.js

--- models/Resume.js
+++ models/Resume.js
@@ -1,39 +1,57 @@
 const mongoose = require("mongoose");
 
-const resumeSchema = new mongoose.Schema(
+const resumeSchema =
+  new mongoose.Schema(
 
-  {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
+    {
+
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      // PDF URL
+      resumeUrl: {
+        type: String,
+        required: true,
+      },
+
+      // Cloudinary Public ID
+      public_id: {
+        type: String,
+        required: true,
+      },
+
+      // File Name
+      fileName: {
+        type: String,
+        default:
+          "Adarsh_Dubey_Resume.pdf",
+      },
+
+      // File Type
+      fileType: {
+        type: String,
+        default: "application/pdf",
+      },
+
+      // File Size
+      fileSize: {
+        type: Number,
+        default: 0,
+      },
+
     },
 
-    resumeUrl: {
-      type: String,
-      required: true,
-    },
+    {
+      timestamps: true,
+    }
 
-    public_id: {
-      type: String,
-      required: true,
-    },
+  );
 
-    fileName: {
-      type: String,
-      default:
-        "Adarsh_Dubey_Resume.pdf",
-    },
-
-  },
-
-  {
-    timestamps: true,
-  }
-
-);
-
-module.exports = mongoose.model(
-  "Resume",
-  resumeSchema
-);
+module.exports =
+  mongoose.model(
+    "Resume",
+    resumeSchema
+  );
